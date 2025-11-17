f77f0431e0d6185c4d6011e14cc4ba78638848af
Fladenz <nicolassiqueiragraciano@gmail.com>
2025-11-17 11:30:34 -0300

    prosseguindo

---
+++ app/src/main/java/com/example/nutrivida/CadastroActivity.java
@@ -0,0 +1,13 @@
+package com.example.nutrivida;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
+
--- app/src/main/java/com/example/nutrivida/MainActivity.java
+++ app/src/main/java/com/example/nutrivida/MainActivity.java
@@ -1,24 +1,41 @@
 package com.example.nutrivida;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        Button loginButton = findViewById(R.id.btn_entrar);
+        Button registerButton = findViewById(R.id.btn_cadastrar);
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.example.nutrivida.LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, com.example.nutrivida.CadastroActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
