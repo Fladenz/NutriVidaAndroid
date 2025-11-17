57673eb4304fe2863223194660d3000d765ce318
Fladenz <nicolassiqueiragraciano@gmail.com>
2025-11-17 16:24:34 -0300

    socomita

--- app/src/main/java/com/example/nutrivida/DashboardActivity.java
+++ app/src/main/java/com/example/nutrivida/DashboardActivity.java
@@ -33,55 +33,55 @@ public class DashboardActivity extends AppCompatActivity {
         welcomeTextView.setText("Olá, " + nomeUsuario + "!");
 
 
-        planoAlimentarButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, PlanoAlimentarActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
-        receitasButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-//                Intent intent = new Intent(DashboardActivity.this, ReceitasActivity.class);
+//        planoAlimentarButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(DashboardActivity.this, PlanoAlimentarActivity.class);
 //                startActivity(intent);
-            }
-        });
+//            }
+//        });
 
 
-        monitoramentoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, MonitoramentoActivity.class);
-                startActivity(intent);
-            }
-        });
+//        receitasButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Intent intent = new Intent(DashboardActivity.this, ReceitasActivity.class);
+////                startActivity(intent);
+//            }
+//        });
 
 
-        configuracoesButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(DashboardActivity.this, ConfiguracoesActivity.class);
-                startActivity(intent);
-            }
-        });
+//        monitoramentoButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(DashboardActivity.this, MonitoramentoActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
-        // declarar e inicializar btnRefeicoesDia antes de usar
-        Button btnRefeicoesDia = findViewById(R.id.btn_refeicoes_dia);
-        btnRefeicoesDia.setOnClickListener(v -> {
-            Intent intent = new Intent(DashboardActivity.this, RefeicoesDiaActivity.class);
-            startActivity(intent);
-        });
+//        configuracoesButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(DashboardActivity.this, ConfiguracoesActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
-        Button btnHidratacao = findViewById(R.id.btn_hidratacao);
-        btnHidratacao.setOnClickListener(v -> {
-            Intent intent = new Intent(DashboardActivity.this, HidratacaoActivity.class);
-            startActivity(intent);
-        });
+//        // declarar e inicializar btnRefeicoesDia antes de usar
+//        Button btnRefeicoesDia = findViewById(R.id.btn_refeicoes_dia);
+//        btnRefeicoesDia.setOnClickListener(v -> {
+//            Intent intent = new Intent(DashboardActivity.this, RefeicoesDiaActivity.class);
+//            startActivity(intent);
+//        });
+
+
+//        Button btnHidratacao = findViewById(R.id.btn_hidratacao);
+//        btnHidratacao.setOnClickListener(v -> {
+//            Intent intent = new Intent(DashboardActivity.this, HidratacaoActivity.class);
+//            startActivity(intent);
+//        });
 
 
     }
